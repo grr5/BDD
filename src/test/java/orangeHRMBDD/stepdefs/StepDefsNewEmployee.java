d413bd27b7f16be021ee8d720b2fb8552ea00c63
Ravi <ravi@gmail.com>
2018-04-20 19:25:12 +0100

    test1

--- src/test/java/orangeHRMBDD/stepdefs/StepDefsNewEmployee.java
+++ src/test/java/orangeHRMBDD/stepdefs/StepDefsNewEmployee.java
@@ -15,7 +15,7 @@ import java.io.IOException;
 public class StepDefsNewEmployee extends BaseTest {
     LoginPage loginPage;
 
-    //Admin Login stepdefs //test
+    //Admin Login stepdefs //test1
 
     @Given("^admin is on login page$")
     public void admin_is_on_login_page() {
